905db37dfead3a0cfa7eb6e0c48b271aaf0f27f6
seriln <isn91@bk.ru>
2012-09-30 23:49:33 +0400

    different url

---
+++ Projects/control_panel/pages/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from datetime import datetime
+from django.shortcuts import render
+import os
+
+def home(request):
+    context={'ts':datetime.now(),}
+    return render(request, 'home.html', context)
+
+def listing(request,dir):
+    dir = '/home/seriln/Projects/'+dir
+    os.chdir(dir)
+#   context = {'dir_content':os.getcwd(),}
+    context = {'dir_content':os.listdir(dir),}
+    return render(request, 'listing.html', context)
